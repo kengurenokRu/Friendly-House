d9c502271d2835ef6c5d134a220137763e4a63bc
kengrenokRu <nastassiadunaeva@gmail.com>
2025-05-18 21:59:24 +0300

    add project

---
+++ js/script.js
@@ -0,0 +1,114 @@
+const callBtn = $('.header__button');
+const callBtn_burger = $('.burger-menu__button');
+const modalCloseBtn = $('.modal-form__close-btn');
+const modalForm = $('.modal-form');
+const header__burger = $('.header__burger');
+const burgerMenu = $('.burger-menu');
+const burgerMenu__linkTtext = $('.burger-menu__link-text');
+const burgerMenu__item = $('.burger-menu__item');
+
+
+function hide_burgerMenu() {
+  if ($('.header__burger').hasClass("header__burger_active")) {
+    $('.burger-menu').hide(500);
+    $('.header__burger-border:nth-child(1)').css({
+      "transform": "rotate(0)"
+    });
+    $('.header__burger-border:nth-child(2)').css({
+      "transform": "rotate(0)"
+    });
+    $('.header__burger-border:nth-child(3)').show();
+    $('.header__burger').toggleClass("header__burger_active");
+  }
+}
+
+burgerMenu__linkTtext.click(function () {
+  hide_burgerMenu();  
+});
+
+burgerMenu__item.click(function () {
+  hide_burgerMenu();  
+});
+
+callBtn.click(function () {
+  hide_burgerMenu();
+  $('.modal-form').show(500);
+});
+
+callBtn_burger.click(function () {
+  hide_burgerMenu();
+  $('.modal-form').show(500);
+});
+
+modalCloseBtn.click(function () {
+  $('.modal-form').hide(500);
+});
+
+$(document).click(function (e) {
+  if ($(e.target).is('.modal-form')) {
+    $('.modal-form').hide(500);
+  }
+  else if ($(e.target).is('.burger-menu') || $(e.target).is('.burger-menu__link-text')) {
+    $('.burger-menu').hide(500);
+  }
+});
+
+header__burger.click(function () {
+  if ($('.header__burger').hasClass("header__burger_active")) {
+    $('.burger-menu').hide(500);    
+  }
+  else {    
+    $('.modal-form').hide(500);
+    $('.burger-menu').show(500);
+  }
+  $('.header__burger').toggleClass("header__burger_active");
+});
+
+
+/*
+
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+
+  navigation: {
+    nextEl: '.slider__button-right',
+    prevEl: '.slider__button-left',
+  },
+});
+
+$('.reservation__form').submit(function (event) {
+  event.preventDefault();
+  $.ajax({
+    url: 'https://jsonplaceholder.typicode.com/posts',
+    type: 'POST',
+    data: $(this).serialize(),
+    dataType: 'html',
+    success(data) {
+      alert('Ваша заявка принята');
+    },
+    error(err){
+      alert('Что-то пошло не так');
+      console.error(err);
+    }
+  })
+});
+
+$('.modal-form__window').submit(function (event) {
+  event.preventDefault();
+  $.ajax({
+    url: 'https://jsonplaceholder.typicode.com/posts',
+    type: 'POST',
+    dataType: 'html',
+    data: $(this).serialize(),
+    success(data) {
+      alert('Ваша заявка принята');
+    },
+    error(){
+      alert('Что-то пошло не так');
+    }
+  })
+  $('.modal-form').hide(500);
+});
+
+*/
