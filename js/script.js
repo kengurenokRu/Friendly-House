908707eb2b55a5aff05d0bf5a231a804c9d84555
Nastassia Dunaeva <nastassiadunaeva@gmail.com>
2025-05-19 16:56:12 +0300

    add task

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-const callBtn = $('.header__button');
-const callBtn_burger = $('.burger-menu__button');
-const modalCloseBtn = $('.modal-form__close-btn');
 const modalForm = $('.modal-form');
 const header__burger = $('.header__burger');
 const burgerMenu = $('.burger-menu');
@@ -8,65 +5,124 @@ const burgerMenu__linkTtext = $('.burger-menu__link-text');
 const burgerMenu__item = $('.burger-menu__item');
 
 
-function hide_burgerMenu() {
-  if ($('.header__burger').hasClass("header__burger_active")) {
-    $('.burger-menu').hide(500);
-    $('.header__burger-border:nth-child(1)').css({
-      "transform": "rotate(0)"
-    });
-    $('.header__burger-border:nth-child(2)').css({
-      "transform": "rotate(0)"
-    });
-    $('.header__burger-border:nth-child(3)').show();
-    $('.header__burger').toggleClass("header__burger_active");
+let prevActiveElement;
+
+
+function addInnert(elem) {
+  prevActiveElement = document.activeElement;
+  for (let i = 0; i < document.body.children.length; i++) {
+    if (document.body.children[i] !== elem) {
+      document.body.children[i].inert = true;
+    }
+  };
+  elem.inert = false;
+  if (elem.closest('[inert]')) elem.closest('[inert]').inert = false;
+
+  for (let i = 0; i < window.elemsInert.length; i++) {
+    if (elem === window.elemsInert[i].elem && window.elemsInert[i].esc) {
+      function esc(e) {
+        if (e.key == 'Escape') {
+          window.elemsInert[i].esc()
+        }
+        document.removeEventListener('keydown', esc);
+      }
+      document.addEventListener('keydown', esc);
+    }
+
   }
 }
 
+function activationInnert(elemsInert) {
+  window.elemsInert = elemsInert;
+
+  function removeInnert(elem) {
+    if (elem) {
+      for (let i = 0; i < document.body.children.length; i++) {
+        if (document.body.children[i] !== elem) {
+          document.body.children[i].inert = false;
+        }
+      };
+      elem.innert = true;
+      prevActiveElement.focus();
+    }
+
+    for (let i = 0; i < elemsInert.length; i++) {
+      if (window.screen.width <= elemsInert[i].breakpoints || !elemsInert[i].breakpoints) {
+        elemsInert[i].elem.inert = true
+        //console.log(elemsInert[i]);
+      }
+    }
+  }
+  removeInnert();
+
+  return removeInnert
+}
+
+const removeInnert = activationInnert([
+    {
+        elem: burgerMenu.get(0),
+        esc: hide_burgerMenu        
+    },
+    {
+        elem: modalForm.get(0),
+        esc: hide_modalForm        
+    }
+]);
+
+function hide_burgerMenu() {  
+    burgerMenu.hide(500);
+    removeInnert(burgerMenu.get(0));    
+}
+
+function hide_modalForm() {
+  modalForm.hide(500);
+  removeInnert(modalForm.get(0));
+}
+
+
 burgerMenu__linkTtext.click(function () {
-  hide_burgerMenu();  
+  hide_burgerMenu();
 });
 
 burgerMenu__item.click(function () {
-  hide_burgerMenu();  
-});
-
-callBtn.click(function () {
   hide_burgerMenu();
-  $('.modal-form').show(500);
 });
 
-callBtn_burger.click(function () {
-  hide_burgerMenu();
-  $('.modal-form').show(500);
+$('.paw-button').click(function () {  
+  modalForm.show(500);
+  addInnert(modalForm.get(0));
 });
 
-modalCloseBtn.click(function () {
-  $('.modal-form').hide(500);
+$('.modal-form__close-btn').click(function () {
+  modalForm.hide(500);
+  removeInnert(modalForm.get(0));
 });
 
 $(document).click(function (e) {
   if ($(e.target).is('.modal-form')) {
-    $('.modal-form').hide(500);
+    modalForm.hide(500);
+    removeInnert(modalForm.get(0));
   }
   else if ($(e.target).is('.burger-menu') || $(e.target).is('.burger-menu__link-text')) {
-    $('.burger-menu').hide(500);
+    burgerMenu.hide(500);
+    removeInnert(burgerMenu.get(0));
   }
 });
 
-header__burger.click(function () {
-  if ($('.header__burger').hasClass("header__burger_active")) {
-    $('.burger-menu').hide(500);    
-  }
-  else {    
-    $('.modal-form').hide(500);
-    $('.burger-menu').show(500);
-  }
-  $('.header__burger').toggleClass("header__burger_active");
+/*
+$('.modal-form__wrapper').click(function () {
+  modalForm.hide(500);
+  removeInnert(modalForm.get(0));
+});*/
+
+
+header__burger.click(function () { 
+    burgerMenu.show(500);
+    addInnert(burgerMenu.get(0)); 
 });
 
 
 /*
-
 const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
   loop: true,
@@ -77,38 +133,6 @@ const swiper = new Swiper('.swiper', {
   },
 });
 
-$('.reservation__form').submit(function (event) {
-  event.preventDefault();
-  $.ajax({
-    url: 'https://jsonplaceholder.typicode.com/posts',
-    type: 'POST',
-    data: $(this).serialize(),
-    dataType: 'html',
-    success(data) {
-      alert('Ваша заявка принята');
-    },
-    error(err){
-      alert('Что-то пошло не так');
-      console.error(err);
-    }
-  })
-});
-
-$('.modal-form__window').submit(function (event) {
-  event.preventDefault();
-  $.ajax({
-    url: 'https://jsonplaceholder.typicode.com/posts',
-    type: 'POST',
-    dataType: 'html',
-    data: $(this).serialize(),
-    success(data) {
-      alert('Ваша заявка принята');
-    },
-    error(){
-      alert('Что-то пошло не так');
-    }
-  })
-  $('.modal-form').hide(500);
-});
-
 */
+
+
